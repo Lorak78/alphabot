2e777b254cdfc54760b97503f68f074946e3d2d6
Karol Bruno <144807930+Lorak78@users.noreply.github.com>
2024-10-04 10:28:09 +0200

    Update client.py

--- alphabot2/client.py
+++ alphabot2/client.py
@@ -1,25 +1,13 @@
-# PROTOCOLLO DEI MESSAGGI INVIATI AL SERVER
-# Usiamo delle stringhe
-# 2 tipi di messaggi: richieste (dal client) e risposte (dal server)
-
-# STRUTTURA RICHIESTE (f-string)
-# f"{command}|{value}" command possibili: forward, backward, left, right
-
-# STRUTTURA RISPOSTE
-# f"{status}|{phrase}" status: ok oppure error, phrase: spiega errore se c'è
-
-# Implementare un client/server tcp che utilizzino questi comandi
-
 import socket
 from pynput import keyboard
 
 SERVER_ADDRESS = ("192.168.1.128", 9090)
 BUFFER_SIZE = 4096
 
-command = ""
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(SERVER_ADDRESS)
 
 def on_press(key):
-    global command
 
     if key.char == "w":
         print("press w")
@@ -30,10 +18,9 @@ def on_press(key):
     elif key.char == "d":
         print("press d")
     
-    command = key.char.lower()
+    s.sendall(key.char.lower().encode())
 
 def on_release(key):
-    global command
 
     if key.char == "w":
         print("release w")
@@ -44,19 +31,17 @@ def on_release(key):
     elif key.char == "d":
         print("release d")
     
-    command = key.char.upper()
+    s.sendall(key.char.upper().encode())
+    
 
 def start_listener():
     with keyboard.Listener(on_press = on_press, on_release = on_release) as listener:
         listener.join()
     
 def main():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(SERVER_ADDRESS)
     start_listener()
     while True:
-        print(f"comando: {command}")
-        s.sendall(command.encode())
+        pass
 
     s.close()
 

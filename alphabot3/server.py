6128aebb5054fa29013432e297cfb83abd95b6f1
Karol Bruno <144807930+Lorak78@users.noreply.github.com>
2024-10-24 13:08:50 +0200

    Update server.py

--- alphabot3/server.py
+++ alphabot3/server.py
@@ -1,12 +1,21 @@
 import socket
 import time
 import AlphaBot
+import sqlite3
 
 MY_ADDRESS = ("10.210.0.138", 9090)
 BUFFER_SIZE = 4096
 TIMEOUT = 10
 
 alphaBot = AlphaBot.AlphaBot()
+
+conn = sqlite3.connect("comandi.db")
+cur = conn.cursor()
+
+dati_tabella = cur.execute("SELECT * FROM tabellaComandi")
+lista_tabella = dati_tabella.fetchall()
+
+diz_comandi_tabella = {elem[0]:elem[1] for elem in lista_tabella}
 
 def main():
     alphaBot.stop()
@@ -54,12 +63,17 @@ def main():
             elif all(not valore for valore in diz_movimenti.values()):
                 print("stop")
                 alphaBot.stop()
+            
 
         except socket.timeout:
             if time.time() - last_message_time > TIMEOUT:
                 print("Timeout: client non ha inviato dati, considerato disconnesso.")
                 alphaBot.stop()
                 break
+        if direz_decode not in diz_comandi_tabella:
+            alphaBot.stop()
+        else:
+            print(diz_comandi_tabella[direz_decode])
 
     connection.close()
 
